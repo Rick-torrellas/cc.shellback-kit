99effd77bd32d57343626c74e776f7593fbc8759
Rick-torrellas <ricardo.torrejas@gmail.com>
2026-03-14 10:58:04 -0400

    fix: arregglar el cambio de nombre de la propiedad de la clase CommandResult

--- src/shellback/core/Shell.py
+++ src/shellback/core/Shell.py
@@ -69,8 +69,8 @@ class Shell(ABC):
             execution_time = time.perf_counter() - start_time
             
             result = CommandResult(
-                stdout=process.stdout,
-                stderr=process.stderr,
+                standand_output=process.stdout,
+                standand_error=process.stderr,
                 return_code=process.returncode,
                 execution_time=execution_time,
                 command_sent=final_args
