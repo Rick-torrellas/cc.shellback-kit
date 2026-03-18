b8f87765de3f71a7a7fb1337869db82f27eb5ba9
Rick-torrellas <ricardo.torrejas@gmail.com>
2026-03-18 04:51:35 -0400

    style: agregar un readme

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from src.shellback import Bash, ConsoleLogger,Command
+from src.CapsuleCore_shellback import Bash, ConsoleLogger,Command
 
 logger = ConsoleLogger()
 mkdir = Command("mkdir").add_args("olis")
 
 with Bash(logger=logger) as sh:
-    sh.run(mkdir)
+    result = sh.run(mkdir)
+    print(result.standard_output)
